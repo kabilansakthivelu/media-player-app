4a23924a1fc3aa655039e7825674ace5f6754339
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-03 19:28:53 +0530

    Navbar menu for desktop view

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import {AiFillHome, AiFillHeart, AiOutlineLogout} from 'react-icons/ai';
 import {BiHeadphone} from 'react-icons/bi';
+import {Link, useHistory} from 'react-router-dom';
+import {auth} from '../../firebase';
 import './Navbar.css';
 
 const Navbar = () => {
 
     const [screen, setScreen] = useState();
+
+    const history  = useHistory();
 
     const screenSize = () =>{
         if(window.innerWidth < 768){
@@ -24,13 +28,19 @@ const Navbar = () => {
         })
     },[])
 
+    const logOut = () => {
+        auth.signOut();
+        history.push("/signin");
+    }
+
     return (
         <div className={screen}>
+        <h1 className="header">Music World</h1>
         <div className="icons">
-            <AiFillHome/>
-            <BiHeadphone/>
-            <AiFillHeart/>
-            <AiOutlineLogout/>
+            <Link to="/"><AiFillHome/></Link>
+            <Link to="/playlists"><BiHeadphone/></Link>
+            <Link to="/favorites"><AiFillHeart/></Link>
+            <AiOutlineLogout onClick={logOut}/>
         </div>
         </div>
     )
