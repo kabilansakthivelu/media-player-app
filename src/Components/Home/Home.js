00c71df7c64b1b310d740b6838bdbfc0f8edc2f5
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-10-04 12:19:46 +0530

    Added content for Songs page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navbar from '../Navbar/Navbar';
+import {db, auth} from '../../firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+import SignIn from '../SignIn/SignIn';
 import './Home.css';
 
 const Home = () => {
+
+    const [user] = useAuthState(auth);
+
+    const [songsList, setSongsList] = useState();
+
+    useEffect(()=>{
+        db.collection('songs').onSnapshot((snapshot)=>{
+        const arr = [];
+        snapshot.forEach((doc)=>{
+            arr.push(doc.data());
+        })
+        setSongsList(arr);
+        })      
+    },[])
+
     return (
         <div>
+            {user ? (
+            <div> 
             <Navbar/>
-            <h1>Home</h1>
+            <div className="songsSection">
+            <h1 className="sectionTitle">Discover your day</h1>
+            <div className="songsDisplay">
+            {songsList.map((song)=>{
+                return(
+                    <div key={song.id} className="singleSong">
+                    <img src={song.imageURL} alt="" className="songImage"/>
+                    <div className="songInfo">
+                    <h1 className="songTitle">{song.title}</h1>
+                    <h1 className="songArtist">{song.artists}</h1>
+                    </div>
+                    </div>
+                )
+            })}
+            </div>
+            </div>
+            </div>
+            )
+            :
+            <SignIn/>
+            }
         </div>
     )
 }
